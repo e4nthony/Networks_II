2cd70e48323af2ea6f0fdc12d296690263e096a7
Anthony E <e4nthony@gmail.com>
2023-05-02 00:36:14 +0300

    comments add

--- src/homework2/test_example2/client.py
+++ src/homework2/test_example2/client.py
@@ -1,13 +1,18 @@
 import socket
 
+# init socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+# handshake with server
 sock.connect(('127.0.0.1', 9999))  # server ip , port
 
 print('successful connection')
 
 while True:
+    # save text to send
     data = input('Enter line:').strip().encode()
-
+    # send
     sock.send(data)
+
+    # receive answer from server
     reply_data = sock.recv(1024)
     print('server reply:', reply_data.decode())
--- src/homework2/test_example2/server.py
+++ src/homework2/test_example2/server.py
@@ -1,14 +1,17 @@
 import socket
 
+# init default public listening socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-
 sock.bind(('0.0.0.0', 9999))  # listen port
 
 sock.listen(1)
 
+# when first pack arrives
 conn, client_address = sock.accept()
-
 print('new connection from', client_address)
+
 while True:
+    # when message from user arrives
     data = conn.recv(1024)
+    # send echo back
     conn.send(b'Echo : ' + data)

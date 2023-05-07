82c0bb092457b73a081248e155c803302d541857
Anthony E <e4nthony@gmail.com>
2023-05-07 17:35:36 +0300

    hw2 update

--- src/homework2/server_standalone.py
+++ src/homework2/server_standalone.py
@@ -5,14 +5,13 @@ others_servers_ip = '127.0.0.1'
 available_ids = [0, 1, 2, 3, 4]
 available_ports = ["4040", "4041", "4042", "4043", "4044"]
 
-# init server id and port
+# init this server id and port
 my_server_id = int(input("enter id of server [0-4]:"))
 my_server_port = int(available_ports[my_server_id])
 
-
-
-# make public socket for incoming requests (handshakes)
+# make public socket for incoming requests (and handshakes)
 public_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+# public_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # listen to port assigned for my server index
 public_sock.bind(('0.0.0.0', my_server_port))
 public_sock.listen(1)
@@ -32,10 +31,12 @@ def hello_from_client(conn_socket, client_address):
     while True:
         # wait for incoming requests from specific socket
         data = conn_socket.recv(1024)  # hello
+
         data_decoded = data.decode()
-        print("recieved from:_", client_address, '_text:_', data_decoded)
+        if data_decoded.replace(" ", "") == "":
+            print("recieved from:", client_address, ', text:_', data_decoded)
         if data_decoded == "hello":
-            print("sending \"hello\"...")
+            print("got \"hello\", sending \"world\"...")
             conn_socket.send("world".encode())
             # conn_socket.close()  # close connection
 
@@ -60,22 +61,31 @@ other_servers_ids = available_ids
 other_servers_ids.pop(my_server_id)
 
 # init sending socket for out-coming requests
-outc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-outc_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # make socket reusable
+# outc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 # outc_sock.bind(('0.0.0.0', int(out_ports[my_server_id])))
 
 # handshake with other servers
 for x in other_servers_ids:
     try:
-        print("trying to connect to : ", others_servers_ip, available_ports[x])
+        # outc_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # make socket reusable
+        # outc_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+        outc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        print("trying to connect to server | id:", x, ", ip:", others_servers_ip, available_ports[x])
         outc_sock.connect((others_servers_ip, int(available_ports[x])))  # server ip str, port int
         # if here then the TCP connection is established
 
+        print("sending \"hello\"")
         outc_sock.send("hello".encode())
+
         # receive answer from server
         reply_data = outc_sock.recv(1024)
-        print("server reply:", reply_data.decode())
-        print("successful connection with server id: ", x)
-        # outc_sock.close()  # close connection
+        print("got reply: \"", reply_data.decode(), "\"")
+        outc_sock.close()  # close connection
+
+        print("connection established successfully with server id: ", x)
+        # break  # to eliminate connection to other
     except ConnectionRefusedError:
         print("ConnectionRefusedError at server id: ", x)
+    except socket.error:
+        print('Failed to reuse socket')  # todo delete
